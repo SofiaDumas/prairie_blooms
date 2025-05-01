16e03b512530d412cfa770c55cefc4ebd1cb239a
Maria Dumas <mdumas2@rrc.ca>
2025-05-01 15:42:43 -0500

    Finalized the payment method

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,11 +6,11 @@ class Admin::OrdersController < ApplicationController
   end
 
   def mark_as_shipped
-    if @order.status == "paid"
-      @order.update(status: "shipped")
-      redirect_to admin_order_path, notice: "Order ##{@order.id} markes as shipped"
+    if @order.paid?
+      @order.update(status: :shipped)
+      redirect_to admin_orders_path, notice: "Order ##{@order.id} markes as shipped"
     else
-      redirect_to admin_order_path, alert: "Only paid orders can be shipped"
+      redirect_to admin_orders_path, alert: "Only paid orders can be shipped"
     end
   end
 
